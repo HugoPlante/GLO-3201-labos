7f5047916dd400f0433e88db87fcc47453189a51
Hugo Plante <hugo.plante.2@ulaval.ca>
2022-09-14 19:55:28 -0400

    fin lab2

--- lab2/main.js
+++ lab2/main.js
@@ -1,42 +1,41 @@
 let dropdown = document.getElementById("dropdown-input");
 let dropdownOptions = document.getElementById("dropdown-options");
+let dropdownInput = document.getElementById("dropdown-input");
 
-dropdown.addEventListener("click", () => {
+const animals = ["Chien 🐶", "Chat 🐱", "Lapin 🐰", "Poisson 🐟", "Oiseau 🐦", "Serpent 🐍", "Tortue 🐢", "Lion 🦁", "Panda 🐼", "Poule 🐔", "Vache 🐮",];
+
+window.addEventListener('load', () => {
+    updateOptions(animals);
+});
+
+dropdownInput.addEventListener("click", () => {
     dropdownOptions.classList.remove("hide");
 });
 
-dropdown.addEventListener("blur", () => {
+dropdownInput.addEventListener("blur", () => {
     dropdownOptions.classList.add("hide");
 });
 
-let dropdownInput = document.getElementById("dropdown-input");
 dropdownInput.addEventListener('input', () => {
     let value = dropdownInput.value;
-
     let filter = value.toLowerCase();
-    let options = Array.from(document.getElementsByClassName("option"));
 
-    options.forEach(option => {
-        let value = option.textContent;
-        if (value.toLowerCase().includes(filter) || value === '') {
-            option.style.display = "block";
-        } else {
-            option.style.display = "none";
-        }
-    });
-    if (options.some(option => option.style.display === "block") === false) {
-        let noOption = document.getElementById("no-option");
-        noOption.style.display = "block";
-    } else {
-        let noOption = document.getElementById("no-option");
-        noOption.style.display = "none";
+    let options = animals.filter(option => option.toLowerCase().startsWith(filter));
+
+    if (options.length === 0) {
+        options = ["Aucun résultat :("];
     }
+
+    updateOptions(options);
 });
 
-document.querySelectorAll('.option').forEach(option => {
-    console.log(option);
-    option.addEventListener('click', () => {
-        console.log(option.textContent);
-        dropdownInput.setAttribute('value', option.textContent);;
-    })
-})
+function updateOptions(options) {
+    dropdownOptions.innerHTML = "";
+
+    options.forEach(option => {
+        let optionElement = document.createElement("div");
+        optionElement.classList.add("option");
+        optionElement.textContent = option;
+        dropdownOptions.appendChild(optionElement);
+    });
+}
