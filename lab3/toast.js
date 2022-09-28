5baa72d73a01ca8855d03d8334e3cfc6b319e274
Hugo Plante <hugo.plante.2@ulaval.ca>
2022-09-28 13:20:17 -0400

    lab3

--- lab3/toast.js
+++ lab3/toast.js
@@ -5,18 +5,44 @@ export class Toast {
         this.container = container;
     }
 
-    toast() {
+    createToastElement() {
         const toast = document.createElement('div');
         toast.className = `toast-${this.type} toast`;
         const toastText = document.createElement('div');
         toastText.className = 'toast-text';
         toastText.textContent = this.text || 'Default Toast';
+        toast.appendChild(this.getIcon());
         toast.appendChild(toastText);
+        return toast;
+    }
+
+    toast() {
+        const toast = this.createToastElement();
 
         this.container.appendChild(toast);
         setTimeout(() => {
             toast.remove()
         }, 3000)
     }
+
+    getIcon() {
+        const icon = document.createElement('img');
+        icon.className = 'icon';
+        switch (this.type) {
+            case 'success':
+                icon.src = './icons/success.svg';
+                return icon;
+            case 'error':
+                icon.src = './icons/error.svg';
+                return icon;
+            case 'info':
+                icon.src = './icons/info.svg';
+                return icon;
+            default:
+                return icon;
+        }
+    }
+
+
 }
 
