62ccc05e4d63cac7b1d15cac8528190d0cd64c86
Hugo Plante <hugo.plante.2@ulaval.ca>
2022-09-28 14:38:42 -0400

    lab4 fin

--- lab4/App.js
+++ lab4/App.js
@@ -1,47 +1,87 @@
-const URL = 'https://glo3102lab4.herokuapp.com/';
+const URL = 'https://glo3102lab4.herokuapp.com';
+const USER_ID = '86cfd5f4-e547-4bac-b3ef-7436bf60250a'
 
 let todoList = document.getElementById('todo-list');
 let addBtn = document.getElementById('add-button');
-addBtn.addEventListener('click', addTask);
 
-document.getElementsByClassName('body')[0].addEventListener('load', getTasks());
+const getTasks = async () => {
+    const response = await fetch(`${URL}/${USER_ID}/tasks`, {
+        method: 'GET',
+    });
 
-async function getTasks() {
-    // const response = await fetch(url, {
-    // method: 'POST', 
-    // });
+    const data = await response.json();
+
+    for (let i = 0; i < data.tasks.length; i++) {
+        let todo = createTodoElement(data.tasks[i].name, data.tasks[i].id);
+        todoList.appendChild(todo);
+    }
 }
 
-function createTodoElement(task) {
+const createTodoElement = (text, id) => {
     let todo = document.createElement('li');
     todo.className = 'todo-item';
-    todo.innerHTML = task;
 
-    todo.addEventListener('click', (event) => editTask(event));
-    todo.addEventListener('blur', (event) => saveEditedTask(event));
+    todo.dataset.id = id;
+
+    let todoText = document.createElement('p');
+    todoText.innerHTML = text;
+    todo.appendChild(todoText);
+
+    let removeBtn = document.createElement('button');
+    removeBtn.innerHTML = '🗑️';
+    removeBtn.addEventListener('click', () => removeTask(event));
+    todo.appendChild(removeBtn);
+
+    todoText.addEventListener('click', (event) => editTask(event));
+    todoText.addEventListener('blur', (event) => saveEditedTask(event));
 
     return todo;
 }
 
-function addTask() {
+const addTask = async () => {
     let text = document.getElementById('new-todo-text').value;
-    let todo = createTodoElement(text);
+    const response = await fetch(`${URL}/${USER_ID}/tasks`, {
+        method: 'POST',
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify({ name: text }),
+    });
+
+    const data = await response.json();
+
+    let todo = createTodoElement(text, data.id);
     todoList.appendChild(todo);
 }
 
-function removeTask(event) {
+const removeTask = async (event) => {
     let todo = event.target.parentElement;
     todoList.removeChild(todo);
+
+    let taskId = todo.dataset.id;
+
+    fetch(`${URL}/${USER_ID}/tasks/${taskId}`, {
+        method: 'DELETE',
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
 }
 
-function editTask(event) {
-    console.log('double click :)');
+const editTask = (event) => {
     let todo = event.target;
     todo.contentEditable = true;
 }
 
-function saveEditedTask(event) {
-    console.log('blur :)');
+const saveEditedTask = async (event) => {
     let todo = event.target;
     todo.contentEditable = false;
+
+    let taskId = todo.parentElement.dataset.id;
+    let text = todo.innerHTML;
+
+    const response = await fetch(`${URL}/${USER_ID}/tasks/${taskId}`, {
+        method: 'PUT',
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify({ name: text }),
+    });
 }
+
+document.getElementsByTagName('body')[0].addEventListener('load', getTasks());
+addBtn.addEventListener('click', addTask);
